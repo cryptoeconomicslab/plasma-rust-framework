ffa0cfc575877ae2fb9b34a052e04195f3b44426
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-07-02 07:10:22 +0900

    Use kvs inside event_db

--- event-watcher/src/event_db.rs
+++ event-watcher/src/event_db.rs
@@ -1,5 +1,5 @@
 use ethabi::Hash;
-use std::collections::HashMap;
+use plasma_db::traits::kvs::{BaseDbKey, KeyValueStore};
 
 pub trait EventDb {
     fn get_last_logged_block(&self, topic_hash: Hash) -> Option<u64>;
@@ -8,49 +8,63 @@ pub trait EventDb {
     fn set_event_seen(&mut self, event_hash: Hash);
 }
 
-#[derive(Default)]
-pub struct DefaultEventDb {
-    last_logged_blocks: HashMap<Hash, u64>,
-    seen_events: HashMap<Hash, bool>,
+pub struct EventDbImpl<KVS> {
+    db: KVS,
 }
 
-impl DefaultEventDb {
-    pub fn new() -> DefaultEventDb {
-        Default::default()
+impl<KVS> From<KVS> for EventDbImpl<KVS>
+where
+    KVS: KeyValueStore<Vec<u8>>,
+{
+    fn from(db: KVS) -> Self {
+        Self { db }
     }
 }
 
-impl EventDb for DefaultEventDb {
+impl<KVS> EventDb for EventDbImpl<KVS>
+where
+    KVS: KeyValueStore<Vec<u8>>,
+{
     fn get_last_logged_block(&self, topic_hash: Hash) -> Option<u64> {
-        match self.last_logged_blocks.get(&topic_hash) {
-            Some(block_number) => Some(*block_number),
-            None => None,
+        match self.db.get(&BaseDbKey::new(topic_hash.0.to_vec())) {
+            Ok(Some(v)) => Some(rlp::decode(&v[..]).unwrap()),
+            Ok(None) => None,
+            Err(_) => None,
         }
     }
 
     fn set_last_logged_block(&mut self, topic_hash: Hash, block_number: u64) {
-        self.last_logged_blocks.insert(topic_hash, block_number);
+        let _ = self.db.put(
+            &BaseDbKey::new(topic_hash.0.to_vec()),
+            &rlp::encode(&block_number),
+        );
     }
 
     fn get_event_seen(&self, event_hash: Hash) -> bool {
-        match self.seen_events.get(&event_hash) {
-            Some(seen) => *seen,
-            None => false,
+        match self.db.get(&BaseDbKey::new(event_hash.0.to_vec())) {
+            Ok(Some(v)) => rlp::decode(&v[..]).unwrap(),
+            Ok(None) => false,
+            Err(_) => false,
         }
     }
 
     fn set_event_seen(&mut self, event_hash: Hash) {
-        self.seen_events.insert(event_hash, true);
+        let _ = self
+            .db
+            .put(&BaseDbKey::new(event_hash.0.to_vec()), &rlp::encode(&true));
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use plasma_db::impls::kvs::memory::CoreDbMemoryImpl;
+    use plasma_db::traits::DatabaseTrait;
 
     #[test]
     fn test_last_logged_block() {
-        let mut db = DefaultEventDb::new();
+        let kvs = CoreDbMemoryImpl::open("kvs");
+        let mut db = EventDbImpl::from(kvs);
         let k = Hash::random();
         assert_eq!(db.get_last_logged_block(k), None);
         db.set_last_logged_block(k, 1);
@@ -59,7 +73,8 @@ mod tests {
 
     #[test]
     fn test_event_seen() {
-        let mut db = DefaultEventDb::new();
+        let kvs = CoreDbMemoryImpl::open("kvs");
+        let mut db = EventDbImpl::from(kvs);
         let k = Hash::random();
         assert_eq!(db.get_event_seen(k), false);
         db.set_event_seen(k);
