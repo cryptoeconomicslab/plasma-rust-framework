48f0a08e27c43a906b51f147231b680ff50fd8af
akihito.nakano <sora.akatsuki@gmail.com>
2019-07-08 22:47:24 +0900

    tweak the variable name to corresponding its type

--- event-watcher/src/event_watcher.rs
+++ event-watcher/src/event_watcher.rs
@@ -81,14 +81,14 @@ where
 
             match self.web3.eth().logs(filter).wait().map_err(|e| e) {
                 Ok(v) => {
-                    let events = self.filter_logs(event, v);
-                    if let Some(last_event) = events.last() {
-                        if let Some(block_num) = last_event.block_number {
+                    let newer_logs = self.filter_logs(event, v);
+                    if let Some(last_log) = newer_logs.last() {
+                        if let Some(block_num) = last_log.block_number {
                             self.db.set_last_logged_block(sig, block_num.low_u64());
                         };
                     };
 
-                    all_logs.extend_from_slice(&events);
+                    all_logs.extend_from_slice(&newer_logs);
                 }
                 Err(e) => {
                     println!("{}", e);
