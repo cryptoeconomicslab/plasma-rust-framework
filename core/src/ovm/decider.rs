33565860ff74cb78932a93503a5dcc0f800a8581
SHUHEI HIYA <syuhei176@gmail.com>
2019-08-02 11:46:17 +0900

    Use generics #133

--- core/src/ovm/decider.rs
+++ core/src/ovm/decider.rs
@@ -3,6 +3,10 @@ use super::Decision;
 pub trait Decider {
     type Input;
     type Witness;
-    fn decide(&self, input: &Self::Input, witness: Self::Witness) -> Decision;
-    fn check_decision(&self, input: &Self::Input) -> Decision;
+    fn decide<D>(&self, input: &Self::Input, witness: Self::Witness) -> Decision<D>
+    where
+        D: Decider;
+    fn check_decision<D>(&self, input: &Self::Input) -> Decision<D>
+    where
+        D: Decider;
 }
--- core/src/ovm/types.rs
+++ core/src/ovm/types.rs
@@ -1,30 +1,37 @@
+use crate::ovm::Decider;
 use bytes::Bytes;
-use ethereum_types::Address;
 
-type DeciderId = Address;
 pub trait Input {}
 pub trait Witness {}
 
+/// The property which will be decided by Decider
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Property {
-    decider: DeciderId,
+pub struct Property<D: Decider> {
+    decider: D,
     input: Bytes,
 }
 
-impl Property {
-    pub fn new(decider: DeciderId, input: Bytes) -> Self {
+impl<D> Property<D>
+where
+    D: Decider,
+{
+    pub fn new(decider: D, input: Bytes) -> Self {
         Property { decider, input }
     }
 }
 
+/// Implication proof element has the property which is decided by Decider
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ImplicationProofElement {
-    implication: Property,
+pub struct ImplicationProofElement<D: Decider> {
+    implication: Property<D>,
     implication_witness: Bytes,
 }
 
-impl ImplicationProofElement {
-    pub fn new(implication: Property, implication_witness: Bytes) -> Self {
+impl<D> ImplicationProofElement<D>
+where
+    D: Decider,
+{
+    pub fn new(implication: Property<D>, implication_witness: Bytes) -> Self {
         ImplicationProofElement {
             implication,
             implication_witness,
@@ -32,20 +39,28 @@ impl ImplicationProofElement {
     }
 }
 
+/// Decision status has 3 state, decided as true or false, or undecided.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum DecisionStatus {
     Decided(bool),
     Undecided,
 }
 
+/// Decision made by Decider
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Decision {
+pub struct Decision<D: Decider> {
     outcome: DecisionStatus,
-    implication_proof: Vec<ImplicationProofElement>,
+    implication_proof: Vec<ImplicationProofElement<D>>,
 }
 
-impl Decision {
-    pub fn new(outcome: DecisionStatus, implication_proof: Vec<ImplicationProofElement>) -> Self {
+impl<D> Decision<D>
+where
+    D: Decider,
+{
+    pub fn new(
+        outcome: DecisionStatus,
+        implication_proof: Vec<ImplicationProofElement<D>>,
+    ) -> Self {
         Decision {
             outcome,
             implication_proof,
