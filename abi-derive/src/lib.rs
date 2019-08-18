9ad53a4278598e91ab934f29f1e855e74b5b01a9
SHUHEI HIYA <syuhei176@gmail.com>
2019-08-18 23:37:44 +0900

    Chore style #147

--- abi-derive/src/lib.rs
+++ abi-derive/src/lib.rs
@@ -11,14 +11,12 @@ use syn;
 #[proc_macro_derive(AbiEncodable)]
 pub fn encodable_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
-
     impl_encodable_macro(&ast)
 }
 
 #[proc_macro_derive(AbiDecodable)]
 pub fn decodable_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
-
     impl_decodable_macro(&ast)
 }
 
@@ -30,33 +28,7 @@ fn impl_encodable_macro(ast: &syn::DeriveInput) -> TokenStream {
             .fields
             .iter()
             .filter(|f| f.attrs.first().is_none())
-            .map(|f| {
-                let ident = &f.ident.clone().unwrap();
-                if let syn::Type::Path(path) = &f.ty {
-                    let type_name = &path.path.segments.first().unwrap().ident;
-                    match &*type_name.to_string() {
-                        "Bytes" => quote! {
-                            Token::Bytes(self.#ident.to_vec())
-                        },
-                        "Integer" => quote! {
-                            Token::Uint(self.#ident.0.into())
-                        },
-                        "H256" => quote! {
-                            Token::FixedBytes(self.#ident.as_bytes().to_vec())
-                        },
-                        "Address" => quote! {
-                            Token::Address(self.#ident)
-                        },
-                        _ => quote! {
-                            Token::Tuple(self.#ident.to_tuple())
-                        },
-                    }
-                } else {
-                    quote! {
-                        Token::Tuple(self.#ident.to_tuple())
-                    }
-                }
-            });
+            .map(create_tokens);
         let gen = quote! {
             impl Encodable for #name {
                 fn to_tuple(&self) -> Vec<Token> {
@@ -84,14 +56,13 @@ fn impl_decodable_macro_for_struct(
     name: &syn::Ident,
     data_struct: &syn::DataStruct,
 ) -> TokenStream {
-    let param_type_list = data_struct
-        .fields
+    let fields = &data_struct.fields;
+    let param_type_list = fields
         .iter()
         .filter(|f| f.attrs.first().is_none())
         .map(create_param_type_token_stream);
-    let mut count: usize = 0;
-    let field_list: &Vec<_> = &data_struct
-        .fields
+    let mut count = -1;
+    let field_list: &Vec<_> = &fields
         .iter()
         .filter(|f| f.attrs.first().is_none())
         .map(move |f| {
@@ -99,19 +70,17 @@ fn impl_decodable_macro_for_struct(
             if let syn::Type::Path(path) = &f.ty {
                 let type_name = &path.path.segments.first().unwrap().ident;
                 count += 1;
-                create_parse_val_token_stream(ident, type_name, count - 1)
+                create_parse_val_token_stream(ident, type_name, count as usize)
             } else {
                 panic!("invalid type")
             }
         })
         .collect();
-    let create_fields = data_struct.fields.iter().map(|f| {
+    let create_fields = fields.iter().map(|f| {
         let is_none = f.attrs.first().is_none();
         if is_none {
             let ident = &f.ident.clone().unwrap();
-            return quote! {
-                #ident
-            };
+            return quote! { #ident };
         } else {
             return quote! {
                 None
@@ -133,6 +102,34 @@ fn impl_decodable_macro_for_struct(
         }
     };
     gen.into()
+}
+
+fn create_tokens(f: &syn::Field) -> proc_macro2::TokenStream {
+    let ident = &f.ident.clone().unwrap();
+    if let syn::Type::Path(path) = &f.ty {
+        let type_name = &path.path.segments.first().unwrap().ident;
+        match &*type_name.to_string() {
+            "Bytes" => quote! {
+                Token::Bytes(self.#ident.to_vec())
+            },
+            "Integer" => quote! {
+                Token::Uint(self.#ident.0.into())
+            },
+            "H256" => quote! {
+                Token::FixedBytes(self.#ident.as_bytes().to_vec())
+            },
+            "Address" => quote! {
+                Token::Address(self.#ident)
+            },
+            _ => quote! {
+                Token::Tuple(self.#ident.to_tuple())
+            },
+        }
+    } else {
+        quote! {
+            Token::Tuple(self.#ident.to_tuple())
+        }
+    }
 }
 
 fn create_param_type_token_stream(f: &syn::Field) -> proc_macro2::TokenStream {
